e87eeab55fff75e14620c30ea789b835922afbf6
Suraj Rana Bhat <ranabhatsuraj093@gmail.com>
2023-10-31 21:38:32 +0200

    some changes

--- app/src/main/java/com/example/poseexercise/MainActivity.kt
+++ app/src/main/java/com/example/poseexercise/MainActivity.kt
@@ -1,11 +1,94 @@
 package com.example.poseexercise
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate")
+        setContentView(R.layout.activity_chooser)
+
+        // Set up ListView and Adapter
+        val listView = findViewById<ListView>(R.id.test_activity_list_view)
+        val adapter = MyArrayAdapter(this, android.R.layout.simple_list_item_2, CLASSES)
+        adapter.setDescriptionIds(DESCRIPTION_IDS)
+        listView.adapter = adapter
+        listView.onItemClickListener = this
+
+    }
+
+
+    private class MyArrayAdapter(
+        private val ctx: Context,
+        resource: Int,
+        private val classes: Array<Class<*>>
+    ) : ArrayAdapter<Class<*>>(ctx, resource, classes) {
+        private var descriptionIds: IntArray? = null
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            var view = convertView
+
+            if (convertView == null) {
+                val inflater = ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                view = inflater.inflate(android.R.layout.simple_list_item_2, null)
+            }
+
+            (view!!.findViewById<View>(android.R.id.text1) as TextView).text =
+                classes[position].simpleName
+            descriptionIds?.let {
+                (view.findViewById<View>(android.R.id.text2) as TextView).setText(it[position])
+            }
+
+            return view
+        }
+
+        fun setDescriptionIds(descriptionIds: IntArray) {
+            this.descriptionIds = descriptionIds
+        }
+    }
+
+
+
+
+    companion object {
+        private const val TAG = "ChooserActivity"
+        private val CLASSES =
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+                arrayOf<Class<*>>(
+                    LivePreviewActivity::class.java,
+                    StillImageActivity::class.java,
+                )
+            else
+                arrayOf<Class<*>>(
+                    LivePreviewActivity::class.java,
+                    StillImageActivity::class.java,
+                    CameraXLivePreviewActivity::class.java,
+                    CameraXSourceDemoActivity::class.java
+                )
+        private val DESCRIPTION_IDS =
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+                intArrayOf(
+                    R.string.desc_camera_source_activity,
+                    R.string.desc_still_image_activity,
+                )
+            else
+                intArrayOf(
+                    R.string.desc_camera_source_activity,
+                    R.string.desc_still_image_activity,
+                    R.string.desc_camerax_live_preview_activity,
+                    R.string.desc_cameraxsource_demo_activity
+                )
     }
 }
