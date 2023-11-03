a67f2820a039f1c24f0fb8fa8711f8a8e5241dae
Giao Ngo <giaongo.97@gmail.com>
2023-11-03 14:23:40 +0200

    design layout for CompletedFragment and CancelFragment

---
+++ app/src/main/java/com/example/poseexercise/views/fragment/CompletedFragment.kt
@@ -0,0 +1,25 @@
+package com.example.poseexercise.views.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.poseexercise.R
+
+class CompletedFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_completed, container, false)
+    }
+
+}
