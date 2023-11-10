0beccca729fcbcca9bec521fe491fbe4ea14f8be
Suraj Rana Bhat <ranabhatsuraj093@gmail.com>
2023-11-10 19:04:53 +0200

    minor changes

--- app/src/main/java/com/example/poseexercise/posedetector/classification/PoseClassifierProcessor.java
+++ app/src/main/java/com/example/poseexercise/posedetector/classification/PoseClassifierProcessor.java
@@ -21,10 +21,13 @@ import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.Looper;
 import android.util.Log;
+
 import androidx.annotation.WorkerThread;
+
 import com.example.poseexercise.data.PostureResult;
 import com.google.common.base.Preconditions;
 import com.google.mlkit.vision.pose.Pose;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -40,125 +43,126 @@ import java.util.Objects;
  * Accepts a stream of {@link Pose} for classification and Rep counting.
  */
 public class PoseClassifierProcessor {
-  private static final String TAG = "PoseClassifierProcessor";
+    private static final String TAG = "PoseClassifierProcessor";
 
-  //private static final String POSE_SAMPLES_FILE = "pose/fitness_pose_samples.csv";
-  private static final String POSE_SAMPLES_FILE = "pose/fitness_poses_csvs_out_v01.csv";
+    //private static final String POSE_SAMPLES_FILE = "pose/fitness_pose_samples.csv";
+    private static final String POSE_SAMPLES_FILE = "pose/fitness_poses_csvs_out_v01.csv";
 
-  // Specify classes for which we want rep counting.
-  // These are the labels in the given {@code POSE_SAMPLES_FILE}. You can set your own class labels
-  // for your pose samples.
-  private static final String PUSHUPS_CLASS = "pushups_down";
-  private static final String SQUATS_CLASS = "squat";
-  private static final String[] POSE_CLASSES = {
-    PUSHUPS_CLASS, SQUATS_CLASS
-  };
+    // Specify classes for which we want rep counting.
+    // These are the labels in the given {@code POSE_SAMPLES_FILE}. You can set your own class labels
+    // for your pose samples.
+    private static final String PUSHUPS_CLASS = "pushups_down";
+    private static final String SQUATS_CLASS = "squat";
+    private static final String[] POSE_CLASSES = {
+            PUSHUPS_CLASS, SQUATS_CLASS
+    };
 
-  private final boolean isStreamMode;
+    private final boolean isStreamMode;
 
-  private EMASmoothing emaSmoothing;
-  private List<RepetitionCounter> repCounters;
-  private PoseClassifier poseClassifier;
-  private String lastRepResult;
+    private EMASmoothing emaSmoothing;
+    private List<RepetitionCounter> repCounters;
+    private PoseClassifier poseClassifier;
+    private String lastRepResult;
 
-  private static final Map<String,PostureResult> postureResults = new HashMap<>();
+    private static final Map<String, PostureResult> postureResults = new HashMap<>();
 
-  @WorkerThread
-  public PoseClassifierProcessor(Context context, boolean isStreamMode) {
-    Preconditions.checkState(Looper.myLooper() != Looper.getMainLooper());
-    this.isStreamMode = isStreamMode;
-    if (isStreamMode) {
-      emaSmoothing = new EMASmoothing();
-      repCounters = new ArrayList<>();
-      lastRepResult = "";
-    }
-    loadPoseSamples(context);
-  }
-
-  private void loadPoseSamples(Context context) {
-    List<PoseSample> poseSamples = new ArrayList<>();
-    try {
-      BufferedReader reader = new BufferedReader(
-          new InputStreamReader(context.getAssets().open(POSE_SAMPLES_FILE)));
-      String csvLine = reader.readLine();
-      while (csvLine != null) {
-        // If line is not a valid {@link PoseSample}, we'll get null and skip adding to the list.
-        PoseSample poseSample = PoseSample.getPoseSample(csvLine, ",");
-        if (poseSample != null) {
-          poseSamples.add(poseSample);
+    @WorkerThread
+    public PoseClassifierProcessor(Context context, boolean isStreamMode) {
+        Preconditions.checkState(Looper.myLooper() != Looper.getMainLooper());
+        this.isStreamMode = isStreamMode;
+        if (isStreamMode) {
+            emaSmoothing = new EMASmoothing();
+            repCounters = new ArrayList<>();
+            lastRepResult = "";
         }
-        csvLine = reader.readLine();
-      }
-    } catch (IOException e) {
-      Log.e(TAG, "Error when loading pose samples.\n" + e);
+        loadPoseSamples(context);
     }
-    poseClassifier = new PoseClassifier(poseSamples);
-    if (isStreamMode) {
-      for (String className : POSE_CLASSES) {
-        repCounters.add(new RepetitionCounter(className));
-      }
+
+    private void loadPoseSamples(Context context) {
+        List<PoseSample> poseSamples = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(context.getAssets().open(POSE_SAMPLES_FILE)));
+            String csvLine = reader.readLine();
+            while (csvLine != null) {
+                // If line is not a valid {@link PoseSample}, we'll get null and skip adding to the list.
+                PoseSample poseSample = PoseSample.getPoseSample(csvLine, ",");
+                if (poseSample != null) {
+                    poseSamples.add(poseSample);
+                }
+                csvLine = reader.readLine();
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error when loading pose samples.\n" + e);
+        }
+        poseClassifier = new PoseClassifier(poseSamples);
+        if (isStreamMode) {
+            for (String className : POSE_CLASSES) {
+                repCounters.add(new RepetitionCounter(className));
+            }
+        }
     }
-  }
 
-  /**
-   * Given a new {@link Pose} input, returns a list of formatted {@link String}s with Pose
-   * classification results.
-   *
-   * <p>Currently it returns up to 2 strings as following:
-   * 0: PoseClass : X reps
-   * 1: PoseClass : [0.0-1.0] confidence
-   */
-  @WorkerThread
-  public Map<String,PostureResult> getPoseResult(Pose pose) {
-    Preconditions.checkState(Looper.myLooper() != Looper.getMainLooper());
-    List<String> result = new ArrayList<>();
+    /**
+     * Given a new {@link Pose} input, returns a list of formatted {@link String}s with Pose
+     * classification results.
+     *
+     * <p>Currently it returns up to 2 strings as following:
+     * 0: PoseClass : X reps
+     * 1: PoseClass : [0.0-1.0] confidence
+     */
+    @WorkerThread
+    public Map<String, PostureResult> getPoseResult(Pose pose) {
+      Preconditions.checkState(Looper.myLooper() != Looper.getMainLooper());
+      List<String> result = new ArrayList<>();
 
-    ClassificationResult classification = poseClassifier.classify(pose);
+      ClassificationResult classification = poseClassifier.classify(pose);
 
-    // Update {@link RepetitionCounter}s if {@code isStreamMode}.
-    if (isStreamMode) {
-      // Feed pose to smoothing even if no pose found.
-      classification = emaSmoothing.getSmoothedResult(classification);
+      // Update {@link RepetitionCounter}s if {@code isStreamMode}.
+      if (isStreamMode) {
+        // Feed pose to smoothing even if no pose found.
+        classification = emaSmoothing.getSmoothedResult(classification);
 
-      // Return early without updating repCounter if no pose found.
-      if (pose.getAllPoseLandmarks().isEmpty()) {
+        // Return early without updating repCounter if no pose found.
+        if (pose.getAllPoseLandmarks().isEmpty()) {
+          result.add(lastRepResult);
+          return postureResults;
+        }
+
+        for (RepetitionCounter repCounter : repCounters) {
+          int repsBefore = repCounter.getNumRepeats();
+          int repsAfter = repCounter.addClassificationResult(classification);
+          if (repsAfter > repsBefore) {
+            // Play a fun beep when rep counter updates.
+            ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
+            tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+            lastRepResult = String.format(
+                    Locale.US, "%s : %d reps", repCounter.getClassName(), repsAfter);
+            // Add result to map
+            postureResults.put(repCounter.getClassName(), new PostureResult(repsAfter, 0));
+            break;
+          }
+        }
         result.add(lastRepResult);
-        return postureResults;
       }
 
-      for (RepetitionCounter repCounter : repCounters) {
-        int repsBefore = repCounter.getNumRepeats();
-        int repsAfter = repCounter.addClassificationResult(classification);
-        if (repsAfter > repsBefore) {
-          // Play a fun beep when rep counter updates.
-          ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
-          tg.startTone(ToneGenerator.TONE_PROP_BEEP);
-          lastRepResult = String.format(
-              Locale.US, "%s : %d reps", repCounter.getClassName(), repsAfter);
-          // Add result to map
-          postureResults.put(repCounter.getClassName(), new PostureResult(repsAfter, 0));
-          break;
-        }
+      // Add maxConfidence class of current frame to result if pose is found.
+      String maxConfidenceClass = null;
+      if (!pose.getAllPoseLandmarks().isEmpty()) {
+        maxConfidenceClass = classification.getMaxConfidenceClass();
+        String maxConfidenceClassResult = String.format(
+                Locale.US,
+                "%s : %.2f",
+                maxConfidenceClass,
+                classification.getClassConfidence(maxConfidenceClass)
+                        / poseClassifier.confidenceRange());
+        result.add(maxConfidenceClassResult);
       }
-      result.add(lastRepResult);
-    }
-
-    // Add maxConfidence class of current frame to result if pose is found.
-    if (!pose.getAllPoseLandmarks().isEmpty()) {
-      String maxConfidenceClass = classification.getMaxConfidenceClass();
-      String maxConfidenceClassResult = String.format(
-          Locale.US,
-          "%s : %.2f",
-          maxConfidenceClass,
-          classification.getClassConfidence(maxConfidenceClass)
-              / poseClassifier.confidenceRange());
-      result.add(maxConfidenceClassResult);
-    }
 
 
-    // writing parsing logic
+      // writing parsing logic
 
-    List<String> data = result;
+    /*List<String> data = result;
 
     String ExerciseRep = data.get(0);
     String PoseConfidence = data.get(1);
@@ -176,7 +180,7 @@ public class PoseClassifierProcessor {
 
       Log.d("Classification Exercise: ", exercise);
       Log.d("Classification Count: ", count);
-    }
+    }*/
 
       // find the key from the map -> if it exists, update the confidence value, otherwise add a new entry
       if (postureResults.containsKey(maxConfidenceClass)) {
@@ -185,20 +189,21 @@ public class PoseClassifierProcessor {
       } else {
         postureResults.put(maxConfidenceClass, new PostureResult(0, classification.getClassConfidence(maxConfidenceClass) / poseClassifier.confidenceRange()));
       }
-    }
-    if (parts.length == 2) {
+
+
+ /*   if (parts.length == 2) {
       // Trim leading and trailing spaces from the parts
       String Expose = parts[0].trim();
       String conf = parts[1].trim();
 
       Log.d("Classification Pose: ", Expose);
       Log.d("Classification Confidence: ", conf);
+    }*/
+
+      //Log.d("Classification: ", PoseConfidence);
+
+      //return result;
+      return postureResults;
     }
-
-    //Log.d("Classification: ", PoseConfidence);
-
-    return result;
-    return postureResults;
-  }
 
 }
