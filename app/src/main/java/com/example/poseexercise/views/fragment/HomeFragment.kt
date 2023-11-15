a5569fa74b6160cbcf920e2b09b81a337767c3a1
Giao Ngo <giaongo.97@gmail.com>
2023-11-16 01:29:50 +0200

    fix UI test error caused by view declaration in onCreateView, recycler view visibility

--- app/src/main/java/com/example/poseexercise/views/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/poseexercise/views/fragment/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.example.poseexercise.views.fragment
 
-import android.content.Context
 import android.os.Bundle
 import android.text.format.DateFormat
 import android.util.Log
@@ -9,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -23,14 +21,12 @@ import com.example.poseexercise.data.results.RecentActivityItem
 import com.example.poseexercise.util.MyApplication
 import com.example.poseexercise.viewmodels.HomeViewModel
 import com.example.poseexercise.viewmodels.ResultViewModel
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.Date
-import kotlin.coroutines.CoroutineContext
 
 
-class HomeFragment : Fragment(), CoroutineScope {
+class HomeFragment : Fragment() {
     val TAG = "RepDetect Home Fragment"
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var resultViewModel: ResultViewModel
@@ -44,11 +40,13 @@ class HomeFragment : Fragment(), CoroutineScope {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val activity = activity as Context
         Log.d(TAG, "TODAY IS $today")
         // Inflate the layout for this fragment
-        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         // Initialize RecyclerView and its adapter for recent activity
         recentActivityRecyclerView = view.findViewById(R.id.recentActivityRecyclerView)
         recentActivityAdapter = RecentActivityAdapter(emptyList())
@@ -78,16 +76,16 @@ class HomeFragment : Fragment(), CoroutineScope {
 
         // Initialize home view model, RecyclerView and its adapter for today's plans
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        val progressText = view.findViewById<TextView>(R.id.exercise_left)
-        val recyclerView= view.findViewById<RecyclerView>(R.id.today_plans)
         val noPlanTV = view.findViewById<TextView>(R.id.no_plan)
         val progressBar = view.findViewById<ProgressBar>(R.id.progress_bar)
         val progressPercentage = view.findViewById<TextView>(R.id.progress_text)
-        val adapter = PlanAdapter(activity)
-        recyclerView.adapter = adapter
+        val adapter = PlanAdapter(this.requireContext())
 
         // get the list of plans from database]
-        launch{
+        lifecycleScope.launch(Dispatchers.Default) {
+            val progressText = view.findViewById<TextView>(R.id.exercise_left)
+            val recyclerView= view.findViewById<RecyclerView>(R.id.today_plans)
+
             planList = homeViewModel.getPlanByDay(today)
             notCompletePlanList = homeViewModel.getNotCompletePlans(today)
             planList?.map {
@@ -96,20 +94,19 @@ class HomeFragment : Fragment(), CoroutineScope {
             planList?.let { adapter.setPlans(it) }
             progressText.text = "${notCompletePlanList?.size} exercise left"
             if(planList?.size == 0){
-                recyclerView.isVisible = false
                 noPlanTV.text = "There is no plan set at the moment"
             }
             if(planList?.size!! > 0  && notCompletePlanList != null){
+                recyclerView.adapter = adapter
+                recyclerView.visibility = View.VISIBLE
                 percentage = ((planList!!.size.minus(notCompletePlanList!!.size))/ planList!!.size)*100
             }
         }
         progressBar.progress = percentage
         progressPercentage.text = percentage.toString()
         Log.d(TAG, "Progress is $percentage")
-        // Return the inflated view
-        return view
-    }
 
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.IO
+    }
 }
+
+
