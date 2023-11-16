56c521da5a1e38eadaa4e751a822d288f43acfc9
Giao Ngo <giaongo.97@gmail.com>
2023-11-16 10:09:17 +0200

    clean the test class

--- app/src/androidTest/java/com/example/poseexercise/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/example/poseexercise/HomeFragmentTest.kt
@@ -6,12 +6,9 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.poseexercise.views.activity.MainActivity
 import com.example.poseexercise.views.fragment.HomeFragment
 import org.hamcrest.CoreMatchers.not
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
